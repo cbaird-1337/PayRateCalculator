420aac1bbf42ce01009bf2c02c2e7d932a365dce
Colin Baird <colinbaird89@gmail.com>
2019-12-27 12:09:44 -0600

    Update paycalc.py

--- paycalc.py
+++ paycalc.py
@@ -6,10 +6,13 @@ h = float(hrs)
 rate = input("Enter rate per hour:")
 r = float(rate)
 
-#factor for overtime if hour input is greater than 40
+#factor for overtime if hour input is greater than 40 and print result
 if h > 40:
     overtime = (h-40)
     overtimeRate = overtime * (1.5*r)
+    print (overtimeRate + (r*40))
 
-#output calculation results of pay * rate
-print (overtimeRate + (r*40))
+#skip the if statement when pay <= 40 and print pay rate at standard rate interval
+else: 
+    print (h * r)
+
