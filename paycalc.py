4968ea42d647a62efa2b64b0ba93e3a9515ee6e2
Colin Baird <colinbaird89@gmail.com>
2019-12-27 11:58:38 -0600

    Create paycalc.py

---
+++ paycalc.py
@@ -0,0 +1,14 @@
+#input hours and convert to floating point integer
+hrs = input("Enter Hours:")
+h = float(hrs)
+
+#input pay rate and convert to floating point integer
+rate = input("Enter rate per hour:")
+r = float(rate)
+
+#factor for overtime if hour input is greater than 40
+if h > 40:
+    overtime = (h-40)
+    overtimeRate = overtime * (1.5*r)
+  
+print (overtimeRate + (r*40))
