bc0977e70b0a52bd01ee0133c0744dce632e6ea8
Colin Baird <colinbaird89@gmail.com>
2019-12-30 13:52:28 -0600

    Update paycalc.py
    added 'try' and 'function' components for better usability

--- paycalc.py
+++ paycalc.py
@@ -1,10 +1,19 @@
 #input hours and convert to floating point integer
-hrs = input("Enter Hours:")
-h = float(hrs)
-
+def hours():
+    try:
+        hrs = input("Enter Hours:")
+        h = float(hrs)
+        return h
+    except:
+        print ("Please enter an integer")
+        hours()
+h = hours()
 #input pay rate and convert to floating point integer
-rate = input("Enter rate per hour:")
-r = float(rate)
+try:
+    rate = input("Enter rate per hour:")
+    r = float(rate)
+except:
+    print ("Please enter an integer")
 
 #factor for overtime if hour input is greater than 40 and print result
 if h > 40:
@@ -13,6 +22,5 @@ if h > 40:
     print (overtimeRate + (r*40))
 
 #skip the if statement when pay <= 40 and print pay rate at standard rate interval
-else: 
+else:
     print (h * r)
-
